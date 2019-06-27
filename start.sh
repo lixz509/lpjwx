2fa922a9f5b17d85f41f3ddfe5f82681e763dd96
lixz509 <lixz509@DESKTOP-6E4S2K8>
2019-06-27 17:32:58 +0800

    ab

--- start.sh
+++ start.sh
@@ -1,17 +1,16 @@
-#! /bin/bash
+﻿#! /bin/bash
 
 # 安装所有模块
 mvn clean install
 
-#启动接入程序
+# 启动接入程序
 cd weixin
-mvn spring-boot:start
+mvn  spring-boot:start
 
-#启动关注程序
+# 启动关注程序
 cd ../subscribe
-mvn spring-boot:start
+mvn  spring-boot:start
 
-#启动取消关注程序
+# 启动取消关注程序
 cd ../unsubscribe
-mvn spring-boot:start
-cd ..
+mvn  spring-boot:start
--- stop.sh
+++ stop.sh
@@ -1,12 +1,13 @@
-#停止接入程序
+﻿#! /bin/bash
+
+# 停止接入程序
 cd weixin
-mvn spring-boot:stop
+mvn  spring-boot:stop
 
-#停止关注程序
+# 停止关注程序
 cd ../subscribe
-mvn spring-boot:stop
+mvn  spring-boot:stop
 
-#停止取消关注程序
+# 停止取消关注程序
 cd ../unsubscribe
-mvn spring-boot:stop
-cd ..
+mvn  spring-boot:stop
